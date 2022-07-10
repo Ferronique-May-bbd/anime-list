c8dd3852a3b9c0d50c721b4c52550c82074dadb3
Ferronique May <ferronique@bbd.co.za>
2022-07-10 19:40:51 +0200

    Added google sign in button. Added db connection to anime db. Adedd user controller.

---
+++ Data/AnimeList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace anime_list.Data
+{
+    public class AnimeList
+    {
+        public int AnimeListId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ScreenTime { get; set; }
+    }
+}
