dfe902a13eb39cad2a990e47c6f625de88400291
Edwin Motlokwa <edwin@bbd.co.za>
2022-07-13 15:49:18 +0200

    Removed Commented code

--- Data/AnimeRating.cs
+++ Data/AnimeRating.cs
@@ -2,15 +2,7 @@
 
 namespace anime_list.Data
 {
-    //public class UserRating
-    //{
-    //    public string UserId { get; set; }
-    //    public int AnimeListId { get; set; }
-    //    public int AnimeRating { get; set; }
-    //    public int UserRatingId { get; set; }
-
-    //}
-
+  
     public class AnimeRatings
     {
         [Key]
